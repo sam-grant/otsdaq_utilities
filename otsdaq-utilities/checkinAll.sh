4b355e3ab75775d259e363f40962d5e0f01f4252
rrivera <rrivera@fnal.gov>
2015-10-26 09:15:08 -0500

    improving checkin all script

--- otsdaq-utilities/checkinAll.sh
+++ otsdaq-utilities/checkinAll.sh
@@ -49,108 +49,25 @@ echo "Status will be logged here: $CURRENT_AWESOME_BASE/checkinAll.log"
 echo
 echo "=================="
 
+echo "log start:" > $CURRENT_AWESOME_BASE/checkinAll.log
 for p in ${REPO_DIR[@]}; do
     if [ -d $p ]; then
 	echo "Checking in $p"
 	cd $p
 	git pull
-	echo "==================" > $CURRENT_AWESOME_BASE/checkinAll.log
+	echo "==================" >> $CURRENT_AWESOME_BASE/checkinAll.log
 	pwd >> $CURRENT_AWESOME_BASE/checkinAll.log
 	git status &>> $CURRENT_AWESOME_BASE/checkinAll.log
 	git commit -m "$1" .
 	git push   
 	cd $CURRENT_AWESOME_BASE
+	echo
+	echo "=================="
+
     fi	   
 done
 
-exit
 
-CURRENT_AWESOME_BASE=$PWD
-if [ "x$OTSDAQ_DIR" == "x" ]; then
-    echo "OTSDAQ_DIR is not defined. Please run setup scripts"
-    return
-fi
-
-if [ -d "$OTSDAQ_DIR" ]; then
-    echo "Checking in $OTSDAQ_DIR"
-    cd $OTSDAQ_DIR
-    git pull
-    echo "==================" > $CURRENT_AWESOME_BASE/checkinAll.log
-    pwd >> $CURRENT_AWESOME_BASE/checkinAll.log
-    git status &>> $CURRENT_AWESOME_BASE/checkinAll.log
-    git commit -m "$1" .
-    git push   
-    cd $CURRENT_AWESOME_BASE
-else
-    echo "Path doesn't exist $OTSDAQ_DIR"
-fi
-
-echo
-echo "=================="
-
-if [ -d "$OTSDAQ_UTILITIES_DIR" ]; then
-    echo "Checking in $OTSDAQ_UTILITIES_DIR"
-    cd $OTSDAQ_UTILITIES_DIR
-    git pull
-    echo "==================" >> $CURRENT_AWESOME_BASE/checkinAll.log
-    pwd >> $CURRENT_AWESOME_BASE/checkinAll.log
-    git status &>> $CURRENT_AWESOME_BASE/checkinAll.log
-    git commit -m "$1" .
-    git push   
-    cd $CURRENT_AWESOME_BASE
-else
-    echo "Path doesn't exist $OTSDAQ_UTILITIES_DIR"
-fi
-
-echo
-echo "=================="
-
-if [ -d "$OTSDAQ_DEMO_DIR" ]; then
-    echo "Checking in $OTSDAQ_DEMO_DIR"
-    cd $OTSDAQ_DEMO_DIR
-    git pull
-    echo "==================" >> $CURRENT_AWESOME_BASE/checkinAll.log
-    pwd >> $CURRENT_AWESOME_BASE/checkinAll.log
-    git status &>> $CURRENT_AWESOME_BASE/checkinAll.log
-    git commit -m "$1" .
-    git push   
-    cd $CURRENT_AWESOME_BASE
-else
-    echo "Path doesn't exist $OTSDAQ_DEMO_DIR"
-fi
-
-echo
-echo "=================="
-
-if [ -d "$OTSDAQ_DEMO_DIR/../otsdaq-firmware" ]; then
-    echo "Checking in $OTSDAQ_DEMO_DIR/../otsdaq-firmware"
-    cd $OTSDAQ_DEMO_DIR/../otsdaq-firmware
-    git pull
-    pwd >> $CURRENT_AWESOME_BASE/checkinAll.log
-    git status &>> $CURRENT_AWESOME_BASE/checkinAll.log
-    git commit -m "$1" .
-    git push   
-    cd $CURRENT_AWESOME_BASE
-else
-    echo "Path doesn't exist $OTSDAQ_DEMO_DIR/../otsdaq-firmware"
-fi
-
-echo
-echo "=================="
-
-if [ -d "$OTSDAQ_DEMO_DIR/../otsdaq-web" ]; then
-    echo "Checking in $OTSDAQ_DEMO_DIR/../otsdaq-web"
-    cd $OTSDAQ_DEMO_DIR/../otsdaq-web
-    git pull
-    echo "==================" >> $CURRENT_AWESOME_BASE/checkinAll.log
-    pwd >> $CURRENT_AWESOME_BASE/checkinAll.log
-    git status &>> $CURRENT_AWESOME_BASE/checkinAll.log
-    git commit -m "$1" .
-    git push   
-    cd $CURRENT_AWESOME_BASE
-else
-    echo "Path doesn't exist $OTSDAQ_DEMO_DIR/../otsdaq-web"
-fi
 
 echo
 echo "=================="
