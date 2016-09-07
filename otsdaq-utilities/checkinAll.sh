8dded2c118f968c35df72aebda46d927587ad203
rrivera <rrivera@fnal.gov>
2016-09-07 14:56:43 -0500

    checkpoint for ConfigurationTree progress

--- otsdaq-utilities/checkinAll.sh
+++ otsdaq-utilities/checkinAll.sh
@@ -83,8 +83,13 @@ echo
 echo "=================="
 
 echo "Git comment '$1'"
-echo "Status will be logged here: $CURRENT_AWESOME_BASE/checkinAll.log"
-
+echo "Status was logged here: $CURRENT_AWESOME_BASE/checkinAll.log"
+echo
+echo "log dump... #######################################################"
+echo
+cat $CURRENT_AWESOME_BASE/checkinAll.log
+echo "end log dump... #######################################################"
+echo "Status was logged here: $CURRENT_AWESOME_BASE/checkinAll.log"
 
 echo
 echo "=================="
