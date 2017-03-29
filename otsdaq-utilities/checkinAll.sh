77a1e3e22aa84ba6e4ff2ccc7d9834cd842ee19f
CMS Test Beam Test Account <cmstestbeam@ftbftracker02.fnal.gov>
2017-03-29 18:05:06 -0500

    UDPDataListenerProducer has right configuration, added CMSTimingInterface

--- otsdaq-utilities/checkinAll.sh
+++ otsdaq-utilities/checkinAll.sh
@@ -4,7 +4,7 @@ echo "======="
 echo "======="
 echo "======="
 echo
-echo "This script was moved to utilities/tools/otsdaq_update.sh"
+echo "This script was moved to otsdaq_utilities/otsdaq-utilities/tools/UpdateOTS.sh"
 echo 
 echo "If you have successfully compiled then you can simply run:"
 echo
