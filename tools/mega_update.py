60310e17f46c591c9fc15e27a337cd167ecab519
Eric Flumerfelt <eflumerf@fnal.gov>
2017-06-05 11:42:03 -0500

    Update script for otsdaq bundle

--- tools/mega_update.py
+++ tools/mega_update.py
@@ -2,12 +2,12 @@
 import os
 import subprocess
 
-artdaqDemoVersion = "s41-e10 prof";
+otsdaqVersion = "s46-e10 prof";
 
 try:
 	os.path.isdir(os.environ['ProductsDir']);
 except KeyError:
-	print "Products variable not set";
+	print "ProductsDir variable not set";
 	exit(1);
 
 ########################################################################
@@ -33,17 +33,17 @@ os.system(cmd);
 ########################################################################
 #pull the latest version of artdaq_demo from scisoft
 ########################################################################
-cmd = 'curl -s http://scisoft.fnal.gov/scisoft/bundles/artdaq_demo/ | grep id=\\"v | grep -oP \'(?<=id=\\")[^\\">]*\'';
+cmd = 'curl -s http://scisoft.fnal.gov/scisoft/bundles/otsdaq/ | grep id=\\"v | grep -oP \'(?<=id=\\")[^\\">]*\'';
 print cmd;
 process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
-artdaq_demo_version,err = process.communicate();
+otsdaq_version,err = process.communicate();
 if(err != '' and not err.find('Total')):
 	print "There was an error executing \"" + cmd + "\"";
 	print "Error:\n" + err;
 
-artdaq_demo_versions = artdaq_demo_version.rstrip().split();
+otsdaq_versions = otsdaq_version.rstrip().split();
 
-#print artdaq_demo_versions;
+#print otsdaq_versions;
 
 ########################################################################
 #detect operating system
@@ -68,10 +68,10 @@ cmd='cd $ProductsDir';
 print cmd
 os.system(cmd)
 
-for artdaq_demo_version in reversed(artdaq_demo_versions):
-	print "Fetching products for artdaq_demo_version: " + artdaq_demo_version;
+for otsdaq_version in reversed(otsdaq_versions):
+	print "Fetching products for otsdaq_version: " + otsdaq_version;
 	print "This step might take longer than you wish so be patient..."
-	cmd = "./pullProducts . " + host_os + " artdaq_demo-" + artdaq_demo_version + " " + artdaqDemoVersion;
+	cmd = "./pullProducts . " + host_os + " otsdaq-" + otsdaq_version + " " + otsdaqVersion;
 	print cmd;
 	process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
 	operatingSystem,err = process.communicate();
@@ -80,54 +80,9 @@ for artdaq_demo_version in reversed(artdaq_demo_versions):
 		print "Error:\n" + err;
 		if( err.find('MANIFEST') == -1):
 			break;
-		print "Trying to fetch an older artdaq_demo_version..."
+		print "Trying to fetch an older otsdaq_version..."
 	else:
 		break;
-
-########################################################################
-#_____MRB
-########################################################################
-cmd = 'curl -s http://scisoft.fnal.gov/scisoft/packages/mrb/ | grep id=\\"v | grep -oP \'(?<=id=\\")[^\\">]*\' | tail -1';
-print cmd
-process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
-mrb_version,err = process.communicate();
-if(err != '' and not err.find('Total')):
-	print "There was an error executing \"" + cmd + "\"";
-	print "Error:\n" + err;
-mrb_version = mrb_version.rstrip();
-#print "-" + mrb_version + "-"
-mrb_period_version = mrb_version[1:].replace('_','.');
-#print "-" + mrb_period_version + "-"
-
-cmd = "curl http://scisoft.fnal.gov/scisoft/packages/mrb/" + mrb_version + "/mrb-" + mrb_period_version + "-noarch.tar.bz2|tar -jx";
-print cmd
-process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
-out,err = process.communicate();
-if(err != '' and not err.find('Total')):
-	print "There was an error executing \"" + cmd + "\"";
-	print "Error: " + err;
-
-
-########################################################################
-#_____XERCES
-########################################################################
-cmd = 'curl -s http://scisoft.fnal.gov/scisoft/packages/xerces_c/ | grep id=\\"v | grep -oP \'(?<=id=\\")[^\\">]*\' | tail -1';
-#print cmd
-process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
-xerces_version,err = process.communicate();
-if(err != '' and not err.find('Total')):
-	print "There was an error executing \"" + cmd + "\"";
-	print "Error:\n" + err;
-xerces_version = xerces_version.rstrip();
-xerces_period_version = xerces_version[1:].replace('_','.');
-
-cmd = "curl http://scisoft.fnal.gov/scisoft/packages/xerces_c/" + xerces_version + "/xerces_c-" + xerces_period_version + "-" + xerces_os + "-x86_64-e10-prof.tar.bz2|tar -jx";
-print cmd
-process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
-out,err = process.communicate();
-if(err != '' and not err.find('Total')):
-	print "There was an error executing \"" + cmd + "\"";
-	print "Error:\n" + err;
 
 ########################################################################
 # Removing all tar files
