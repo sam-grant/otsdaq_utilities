c837c243b0fe07123666b40aa52c69f95e407ffb
Mauro Dinardo <mauro.dinardo@cern.ch>
2020-01-23 10:13:33 -0600

    Added threaded management of histograms

--- otsdaq-utilities/Visualization/VisualDataManager.h
+++ otsdaq-utilities/Visualization/VisualDataManager.h
@@ -35,6 +35,7 @@ class VisualDataManager : public DataManager
 	// Getters
 	DQMHistosBase* getLiveDQMHistos(void);
 	DQMHistosBase& getFileDQMHistos(void);
+	void           setWait(bool wait){wait_ = wait;}
 	// const Visual3DEvents&   getVisual3DEvents   (void);
 	// const Visual3DGeometry& getVisual3DGeometry (void);
 
@@ -48,6 +49,7 @@ class VisualDataManager : public DataManager
 	// Visual3DData           the3DData_;
 
 	RawDataVisualizerConsumer* theRawDataConsumer_;
+	bool           wait_;
 
 };
 }
