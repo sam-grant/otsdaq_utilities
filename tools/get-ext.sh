7e0f12034f361ece26d2d4ad04f6a26ccd8d5d6f
Eric Flumerfelt <eflumerf@fnal.gov>
2020-03-02 10:50:24 -0600

    Testing removal of extjsLib

---
+++ tools/get-ext.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! [ -d $OTSDAQ_UTILITIES_DIR ]; then
+    echo "You must first set up your otsdaq environment!"
+    exit
+fi
+
+cd $OTSDAQ_UTILITIES_DIR/WebGUI
+curl -o ext-6.2.0-gpl.zip https://otsdaq.fnal.gov/downloads/ext-6.2.0-gpl.zip
+unzip ext-6.2.0-gpl.zip
+rm ext-6.2.0-gpl.zip
+
+
