125ba32c9a89e4ec132df599dfa98dcd6950762d
Eric Flumerfelt <eflumerf@fnal.gov>
2020-03-02 10:56:10 -0600

    Overwrite files without asking

--- tools/get-ext.sh
+++ tools/get-ext.sh
@@ -7,7 +7,7 @@ fi
 
 cd $OTSDAQ_UTILITIES_DIR/WebGUI
 curl -o ext-6.2.0-gpl.zip https://otsdaq.fnal.gov/downloads/ext-6.2.0-gpl.zip
-unzip ext-6.2.0-gpl.zip
+unzip -o ext-6.2.0-gpl.zip
 rm ext-6.2.0-gpl.zip
 
 
